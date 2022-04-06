d6a26708c1b6d1e8150a7f9afa5af7bba1266e12
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-04-06 14:31:48 +0600

    Console Logs, Errors, Warnings & More-2

---
+++ Console Logs, Errors, Warnings & More-2/js/app.js
@@ -0,0 +1,51 @@
+//ekta string print korbe (hello)
+console.log("hello");
+
+ //4+34 er jogfoll console dekhabe 
+console.log(4+34);
+
+//number ke print korbe jehetu ("") deoya nai 
+console.log(34);
+
+//ture print korbe console a . r ekhane true holo booling data type
+console.log(true);
+
+//eka array list print korbe 
+console.log([3, 5, 6, 7, 8, 9]);
+
+//object print korbe console a 
+console.log({Oliul: "this", mark: 34});
+
+//object print korbe but table akare console a 
+console.table({Oliul: "this", mark: 34});
+
+//console output a warning akare dekhabe 
+console.warn("tihs is a warnning");
+
+//console er sob data clear kore dibe
+//console.clear();
+
+// majher code gula likhte koto somoy lagche ta dekhabe
+console.time("my code type time");
+
+console.log("oliul alam");
+console.log("shapla");
+console.log("jibon");
+console.log("fahim");
+console.log("shuvo");
+
+console.timeEnd("total time");
+
+//condition jodi true na hoy to error akre dekhabe 
+console.assert(566<185 ,"age>185 not possible")
+
+// console error show korate 
+console.error("this is a error");
+
+//inline comment
+
+/*
+ miltipulle line comment
+ comment gule ke code editior node akare ney
+*/
+
