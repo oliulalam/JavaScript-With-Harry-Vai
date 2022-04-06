f9a54717d73a5f8cb6de63723f4f768fe590020d
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-04-07 00:40:41 +0600

    Data Types in JavaScript (Primitive & Reference Types)

---
+++ 3-Variables let, const & var in JavaScript/index.js
@@ -0,0 +1,15 @@
+ //variable holo ek dhoroner container ba patthoro jekhane kichu store kore rakha jay
+
+ var name ="Oliul";
+
+ //ekhane name holo ekta container ba pattro jar majhe Oliul name string ke store kore rakha hoyeche 
+
+ // var , const, let, ei 3 ta keyword diye variable declare kora hoy
+
+ let city = "oliul";
+ // city er nam jodi change korte chan tahole
+  city = "Shapla";
+
+  const city2 = "rampura";
+  // const er variable value fixd thake ja change kora jay na 
+  
---
+++ 4-Data Types in JavaScript (Primitive & Reference Types)/index.js
@@ -0,0 +1,49 @@
+//Primitive data type
+
+//string
+let name = "oliul";
+console.log(name);
+console.log(typeof name);
+
+//boolean 
+let isDriver = true;
+console.log(isDriver);
+console.log(typeof isDriver);
+
+//Null 
+//null data type object type dekhaleo asole eta primitive type
+let nulVar = null;
+console.log(nulVar);
+console.log(typeof nulVar);
+
+//undefined
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+
+//Reference Data type 
+
+//Arrays
+let myArray = [34, 22, 11, 66, 88, "string"];
+console.log(myArray);
+console.log(typeof myArray);
+
+//object literats
+let stMarks ={
+    harry: 45,
+    oliul: 89,
+    fahim: 56,
+    shuvo: 45
+
+}
+console.log(stMarks);
+console.log(typeof stMarks);
+
+function nameFun() {
+    
+}
+console.log(typeof nameFun);
+
+let date = new Date();
+console.log(typeof date);
