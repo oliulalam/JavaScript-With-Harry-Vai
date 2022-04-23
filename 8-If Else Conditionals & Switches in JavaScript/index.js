a1fad0f2f6e2a353691659ac1f45401bc19c61a2
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-04-24 02:41:38 +0600

    If Else Conditionals & Switches in JavaScript

---
+++ 8-If Else Conditionals & Switches in JavaScript/index.js
@@ -0,0 +1,31 @@
+console.log("tut 8");
+
+
+// const age= 65;
+
+// if (age==10){
+//     console.log("age is 10")
+// }
+// else if(age==65){
+
+//     console.log("age is 65")
+// }
+// else{
+//     console.log("age is not 10")
+// }
+
+//Age er value er type hocche string
+const age= "65";
+if (age==10){
+    console.log("age is 10")
+}
+// (===) ei operator er kaj holo type and value check kora jodi 2 tai match kore to age is 65 print korbe .. r jodi match na kore tahole age is not 65 print korbe
+
+// amader condition er sathe match kore tai print korbe age is 65 
+else if(age==="65"){
+
+    console.log("age is 65")
+}
+else{
+    console.log("age is not 10")
+}
