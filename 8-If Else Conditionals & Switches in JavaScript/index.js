d29cc8522f9a41f1f5eee29556d5b5c56920f1ed
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-04-25 22:47:24 +0600

    If Else Conditionals & Switches in JavaScript

--- 8-If Else Conditionals & Switches in JavaScript/index.js
+++ 8-If Else Conditionals & Switches in JavaScript/index.js
@@ -29,3 +29,64 @@ else if(age==="65"){
 else{
     console.log("age is not 10")
 }
+
+var notEqual = 35;
+// (!=) not equal
+// (!==) not double equal -> value and type is not matching 
+if (notEqual !==34){
+  console.log(" not equal 34");// condition false hoyche 
+}
+else{
+    console.log("equal 34")
+}
+
+//variable defined kora ache ki na ta check korte 
+
+var vari = 23;// var jodi define kora thake to vari is defined dekhabe r na hole else chole jabe.
+if (typeof vari !== "undefined"){
+    console.log("vari is defined");
+}
+else {
+    console.log("var is not defined");
+}
+
+// true false check korte 
+var doseDrive= true; //jodi amader condition false hoy to tahole else a chole jabe 
+if(doseDrive){
+    console.log("you can drive");
+}
+else{
+    console.log("you can not drive");
+}
+
+var age1 = 14;
+var doseDrive1 = true;
+// if(doseDrive1 || age1>18) // (||)-> jekono ekta condition true hole print korbe
+if(doseDrive1&&age1>18) // 2 ta condition true hote hobe
+{
+    console.log("you can drive");
+}
+else{
+    console.log("you can not drive")
+}
+
+// Ternary operator
+var age2 = 44;
+console.log(age2 == 45? "age is 45" : "age is not 45");
+
+// Switch case
+var age3 = 19;
+switch (age3){
+    case 18:
+        console.log("you are 18");
+        break;
+    case 11:
+        console.log("you are 10");
+        break;
+    case 19:
+        console.log("you are 19");
+        break;
+    default:
+        console.log("you are unknown age");
+        break;
+}
