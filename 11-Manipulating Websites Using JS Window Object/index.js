cb366b54bf0626d5adb5f3087a4c5aba68430220
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-05-03 14:06:18 +0600

    Manipulating Websites Using JS Window Object

---
+++ 11-Manipulating Websites Using JS Window Object/index.js
@@ -0,0 +1,27 @@
+// let a = window;
+// console.log(a);
+
+//alert("hi this is joy");
+//confirm("are you sure");
+// prompt("Enter your name");
+
+// let b =document;
+// console.log(b);
+
+// let c = document.button;
+// console.log(c);
+
+let a = innerHeight;
+console.log(a);
+
+let b = innerWidth;
+console.log(b);
+
+let c = scrollX;
+console.log(c);
+
+let v = scrollY;
+console.log(v);
+
+let r = document.reload;
+console.log(r);
