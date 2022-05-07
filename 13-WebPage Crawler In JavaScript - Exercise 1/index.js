831c1bcf831e49cd46473835a2b1c775ef8e3646
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-05-08 00:02:58 +0600

    WebPage Crawler In JavaScript - Exercise 1

---
+++ 13-WebPage Crawler In JavaScript - Exercise 1/index.js
@@ -0,0 +1,9 @@
+let a = "oliul";
+a = document.links[0].href;
+b = document.links[2].href;
+c = document.links[3].href;
+d = document.links[4].href;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
