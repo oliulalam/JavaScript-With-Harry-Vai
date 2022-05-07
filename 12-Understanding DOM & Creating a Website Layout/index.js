2909e1b48c30dc5f38b6b3c2f57aa7bf1c176a4f
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-05-07 23:23:22 +0600

    Understanding DOM & Creating a Website Layout

---
+++ 12-Understanding DOM & Creating a Website Layout/index.js
@@ -0,0 +1,14 @@
+let a = document;//html tag gula ke pabo 
+// a= document.all; //html er all tag gula ke console dibe 
+// a = document.forms[0]; // html er majhe joto forms tag ache ta pabo 
+
+// jodi array akare element gula ke console a pete chai 
+// Array.forms(a).forEach(function(element))
+// {
+    //     console.log(element);
+    // }
+
+    // a=document.links[0].href;//html er majhe joto link tag ache ta pabo 
+
+    
+    console.log(a);
