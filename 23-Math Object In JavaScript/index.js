7ad01ab84eda2cbde71e8e48cc75a0f731313e3e
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-06-01 01:24:05 +0600

    Math Object In JavaScript

---
+++ 23-Math Object In JavaScript/index.js
@@ -0,0 +1,38 @@
+let x = 3;
+let y = 5;
+let z;
+
+z =x+y;
+z =x-y;
+z =x*y;
+z =x/y;
+
+//math object dekhte 
+z = Math;
+//PI  er man dekhte 
+z = Math.PI;
+//Owler number dekhte
+z = Math.E;
+//5.6 ke 6 akare dekhte /ceil korte 
+z = Math.ceil(5.6);
+//5.5 ke round figer 5 korte / round korte 
+z = Math.round(5.5);
+//5.5 ke 5 korte / floor korte 
+z = Math.floor(5.5);
+//sob somoy positive number print korte .apni jodi -5 o den take positive number a print korbe 
+z = Math.abs(-5);
+//5 er route man dekhte 
+z = Math.sqrt(5);
+//2 to the power 3 er result dekhte 
+z = Math.pow(2,3);
+//ekta list er majhe sob theke choto number dekhte 
+z = Math.min(2, 4, 5, 6, 7);
+//ekta list er majhe sob theke boro number dekhte 
+z = Math.max(2, 3, 4, 5, 6, 7);
+//random number dekhte . 0-100 er majhe random number dekhabe
+z = 100*Math.random();
+//50 theke 100 er majhe random number dekhabe
+z = 50+(100+50).Math.random();
+//50.7866 .er pore kono number pete na chaile
+z = Math.ceil(50+(100-500)).Math.random();
+console.log(z);
