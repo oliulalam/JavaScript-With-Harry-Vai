90e84f476ea19115c871dcca1988eba773499415
Oliul Alam <67019458+oliulalam@users.noreply.github.com>
2022-06-02 01:49:46 +0600

    Object Prototype In javascript

---
+++ 28-Object Prototype In javascript/apps.js
@@ -0,0 +1,14 @@
+//object literal object:prototype
+//sokol javaScript object tader property anad method prototype theke pay.
+//object literal othoba new object ()  dara banano object:prototype theke inherit kore.
+let obj ={
+    name : "Oliul",
+    channle : "It Shikkha Ghor",
+    address : "youtube"
+}
+
+function Obj(givenName){
+    this.name = givenName;
+} 
+let obj2 = new Obj("oliul");
+console.log(obj2);
